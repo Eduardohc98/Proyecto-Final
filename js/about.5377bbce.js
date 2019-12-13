b9e31350b5ae444d6c799c2bf1bf556772f15092
Eduardo Hidalgo <eduardo.hidalgo.crespo@gmail.com>
2019-12-13 14:14:26 -0500

    avance proyecto-final

---
+++ js/about.5377bbce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{bfc1:function(n,a,c){"use strict";c.r(a);var e=function(){var n=this,a=n.$createElement,c=n._self._c||a;return c("div",{staticClass:"cocina"},[c("Cocina")],1)},t=[],o=c("e3a6"),i={name:"cocina",components:{Cocina:o["a"]}},s=i,l=c("2877"),u=Object(l["a"])(s,e,t,!1,null,null,null);a["default"]=u.exports}}]);
+//# sourceMappingURL=about.5377bbce.js.map
